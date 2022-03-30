0dd20b197d852bdca08e92bb2dc42935c694374d
simon <scriado.f@gmail.com>
2022-03-30 17:12:14 +0200

    Initial setup of weather app following an Udemy JS course

---
+++ scripts/forecast.js
@@ -0,0 +1,17 @@
+const key = 'XEvUQAAGSrFGgmN6okMd5c9AmskrYAru'
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+getCity('manchester')
+.then(data => console.log(data))
+.catch(err => console.log(err));

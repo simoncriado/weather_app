865aebc380d2da6ebabbc2741480f549c16aba57
simon <scriado.f@gmail.com>
2022-03-31 10:48:52 +0200

    Setup API call and injecting the data to the UI, some styling and conditionally adding images and icons to UI depending on day/night and weather condition

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,21 @@
 const key = 'XEvUQAAGSrFGgmN6okMd5c9AmskrYAru'
 
+
+// Get weather information
+const getWeather = async (id) => {
+
+const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+const query = `${id}?apikey=${key}`;
+
+const response = await fetch(base+query);
+const data = await response.json();
+
+return data[0];
+
+}
+
+
+// Get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -11,7 +27,3 @@ const getCity = async (city) => {
     return data[0];
 
 };
-
-getCity('manchester')
-.then(data => console.log(data))
-.catch(err => console.log(err));
